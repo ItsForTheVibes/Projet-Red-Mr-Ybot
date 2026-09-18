c36c10ffb1902417bbed49b280bf54469926f0c8
laadrien <adrien.lavigne@ynov.com>
2026-09-18 12:22:16 +0200

    takePot

--- src/combat/combat.go
+++ src/combat/combat.go
@@ -1,1 +1,23 @@
 package combat
+
+import "honnef.co/go/tools/printf"
+
+func takePot(player *Player) { // Vérifier qu'il y a une potion dans l'inventaire
+
+	for i, item := range player.Inventory {
+		if item == "potion" {
+			player.Inventory = append(player.Inventory[:i], player.Inventory[i+1:]...)
+
+			player.CurrentHP += 50
+
+			if player.CurrentHP > player.MaxHP {
+				player.CurrentHP = player.MaxHP
+			}
+
+			printf("Vous avez utilisez la potion !\n Points de vie : %d/%d\n", player.CurrentHP, player.MaxHP)
+
+			return
+		}
+	}
+	println("Vous n'avez pas de potion.")
+}
