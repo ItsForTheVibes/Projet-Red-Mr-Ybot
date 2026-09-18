aa488c9f48a3b89f8a20b7d44cee6a7b83d46e5c
mistergaming111-netizen <mistergaming111@gmail.com>
2026-09-18 15:42:58 +0200

    ajout quete 11

--- src/character/character.go
+++ src/character/character.go
@@ -1,5 +1,10 @@
 package character
 
+import (
+	"fmt"
+	"strings"
+	"unicode"
+)
 
 type Character struct {
 	Name      string   
@@ -20,4 +25,62 @@ func InitCharacter(name string, class string, maxhp int) *Character {
 		CurrentHP: maxhp / 2,                    
 		Inventory: []string{"Potion", "Potion"}, 
 	}
+}
+
+func CharacterCreation() *Character {
+	var name string
+	var choice int
+	var class string
+	var maxHP int
+
+	
+	for {
+		fmt.Print("Entrez le nom de votre personnage : ")
+		fmt.Scanln(&name)
+
+		valid := len(name) > 0
+		for _, char := range name {
+			if !unicode.IsLetter(char) {
+				valid = false
+				break
+			}
+		}
+
+		if valid {
+			name = strings.ToLower(name)
+			runes := []rune(name)
+			runes[0] = unicode.ToUpper(runes[0])
+			name = string(runes)
+			break
+		}
+		fmt.Println("Erreur : le nom ne doit contenir que des lettres !")
+	}
+
+	
+	for {
+		fmt.Println("Choisissez votre classe :")
+		fmt.Println("1. Humain (100 PV)")
+		fmt.Println("2. Elfe (80 PV)")
+		fmt.Println("3. Nain (120 PV)")
+		fmt.Print("Choix (1-3) : ")
+		fmt.Scanln(&choice)
+
+		if choice == 1 {
+			class = "Humain"
+			maxHP = 100
+			break
+		} else if choice == 2 {
+			class = "Elfe"
+			maxHP = 80
+			break
+		} else if choice == 3 {
+			class = "Nain"
+			maxHP = 120
+			break
+		}
+		fmt.Println("Choix invalide !")
+	}
+
+	
+	return InitCharacter(name, class, maxHP)
 }
