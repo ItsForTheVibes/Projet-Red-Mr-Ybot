d88ff8936c15d0541c9ccae1c6a4a1f13392592f
mistergaming111-netizen <mistergaming111@gmail.com>
2026-09-18 11:32:44 +0200

    quete 2 finie

--- src/character/character.go
+++ src/character/character.go
@@ -9,3 +9,14 @@ type Character struct {
 	CurrentHP int      // Points de vie actuels (Nombre entier)
 	Inventory []string // Inventaire sous forme de liste de chaînes de caractères (slice)
 }
+
+func InitCharacter (name string,class string,level int,maxhp int,currenthp int,inventory []string) Character{
+    return Character{
+        Name : name,
+        Class : class,
+        Level : level,
+        MaxHP : maxhp,
+        CurrentHP : currenthp,
+        Inventory : inventory,
+    }
+}
